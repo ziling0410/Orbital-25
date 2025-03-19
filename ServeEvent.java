7fe84f8bbbe98c17113aa0316967024d2fc236a9
ziling0410 <53254000+ziling0410@users.noreply.github.com>
2025-03-19 20:27:24 +0800

    Update ServeEvent.java

--- ServeEvent.java
+++ ServeEvent.java
@@ -2,17 +2,28 @@ import java.util.Optional;
 
 class ServeEvent extends Event {
     private final Server server;
+    private final boolean wasWaiting;
+    private final double serveTime;
 
-    ServeEvent(Customer customer, Server server, double eventTime) {
+    ServeEvent(Customer customer, Server server, double eventTime, 
+            boolean wasWaiting, double serveTime) {
         super(customer, eventTime);
         this.server = server;
+        this.wasWaiting = wasWaiting;
+        this.serveTime = serveTime;
     }
 
     public Optional<Pair<Event,Shop>> next(Shop shop) {
-        double newEventTime = super.eventTime + shop.getServiceTime();
-        Shop newShop = shop.update(super.customer, this.server);
-        return Optional.of(new Pair<Event,Shop>(new DoneEvent(super.customer, 
-                    newEventTime), newShop));
+        //double newEventTime = super.eventTime + shop.getServiceTime();
+        Optional<Server> newServer = shop.getServer(this.server);
+        Optional<Shop> newShop = Optional.of(shop);
+        if (this.wasWaiting) {
+            newServer = newServer.map(x -> x.removeFromQueue());
+            newShop = newServer.map(x -> shop.removeQueue(x));
+        }
+        //newShop = shop.update(super.customer, newServer, newEventTime);
+        return newShop.map(x -> new Pair<Event,Shop>(new DoneEvent(
+                        super.customer, this.serveTime), x));
     }
     
     @Override
