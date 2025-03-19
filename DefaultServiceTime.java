0ef8de7a3149745532e0c40aff8a28bb9a087b1f
ziling0410 <53254000+ziling0410@users.noreply.github.com>
2025-03-19 20:26:02 +0800

    Update DefaultServiceTime.java

--- DefaultServiceTime.java
+++ DefaultServiceTime.java
@@ -2,6 +2,7 @@ import java.util.function.Supplier;
 
 class DefaultServiceTime implements Supplier<Double> {
     public Double get() {
+        System.out.println("get");
         return 1.0;
     }
 }
