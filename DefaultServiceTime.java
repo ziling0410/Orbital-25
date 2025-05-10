aca7d6a2525a43f76a1e124be76deda396264348
ziling0410 <53254000+ziling0410@users.noreply.github.com>
2025-05-10 14:54:00 +0800

    Delete DefaultServiceTime.java

--- DefaultServiceTime.java
+++
@@ -1,9 +0,0 @@
-import java.util.function.Supplier;
-
-class DefaultServiceTime implements Supplier<Double> {
-    public Double get() {
-        System.out.println("get");
-        return 1.0;
-    }
-}
-
