9f9b88b7bbb9736a13657ba0ce492015d31b24d7
ziling <phoonziling@gmail.com>
2025-06-29 23:08:23 +0800

    Minor styling updates

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -85,7 +85,44 @@ function Home() {
     };
 
     if (!userProfile) {
-        return null;
+        return (
+            <div className="home-entire">
+                <div className="home-top">
+                    <div className="home-top-left" onClick={handleHomeClick}>
+                        <h1 className="home-brand">MERCHMATES</h1>
+                    </div>
+                    <div className="home-top-center">
+                        <SearchBar
+                            searchInput={searchInput}
+                            setSearchInput={setSearchInput}
+                            handleSearch={handleSearch}
+                            handleClearSearch={handleClearSearch}
+                        />
+                    </div>
+                    <div className="home-top-right-new">
+                        <button className="button" onClick={() => navigate("/register")}>Register</button>
+                        <button className="button" onClick={() => navigate("/login") }>Login</button>
+                    </div>
+                </div>
+                <div className="home-bottom">
+                    <div className="home-bottom-left">
+                        <div className="home-bottom-left-top">
+                            <img src="/home.png" alt="Icon"></img>
+                            <h1 className="reborn">MERCHMATES</h1>
+                        </div>
+                        <div className="home-bottom-left-bottom">
+                            <p className="hero">CONNECTING FANS WORLDWIDE WITH A SIMPLE CLICK</p>
+                        </div>
+                    </div>
+                    <div className="home-bottom-right">
+                        <div className="home-bottom-right-box">
+                            <button className="trade" onClick={handleTrade}>Trade</button>
+                            <button className="trade" onClick={handleNotifications}>Notifications</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );;
     }
 
     return (
